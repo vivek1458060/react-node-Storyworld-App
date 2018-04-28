212c4a8444f6d4da280e355c2fd8a66d972f47b3
unknown <vivek1458060@gmail.com>
2018-04-28 19:06:40 +0530

    =add header resposive dropdown for Chatroom join/create

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,43 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-import {connect} from 'react-redux';
-import {startLogout} from '../actions/auth';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { startLogout } from '../actions/auth';
 
-export const Header = ({startLogout}) => (
+export const Header = ({ startLogout }) => (
     <header className="header">
-        <div className="content-container">
-            <div className="header__content">
-                <Link to="/dashboard" className="header__title">
+      <div className="content-container">
+        <div className="header-content">
+            <div className="header-main">
+                <Link to="/dashboard" className="header-main__title">
                     <h1>Storyclub</h1>
                 </Link>
-                <button className="button button--link" onClick={startLogout}>Logout</button>
+                <span className="header-main__icon display-for-mobile">&#9776;</span>
             </div>
-        </div>
+            <ul className="header-sub">
+                <li className="dropdown">
+                    <button className="button button--link" >Chat room</button>
+                    <div className="dropdown__content">
+                        <div className="room-actions">
+                            <h3>Join Chat Rooms</h3>
+                            <button>Create new</button>
+                        </div>
+                        <input className="room-search" type="text" placeholder="Search chat rooms" />
+                        <ul className="room-list">
+                            <li>
+                                <h4>Heritage talent<button>Join</button></h4>
+                                <span><img src="https://avatars0.githubusercontent.com/u/26858253?v=4" />1 member</span>
+                            </li>
+                            <li>
+                                <h4>Chhapra<button>Join</button></h4>
+                                <span><img src="https://avatars0.githubusercontent.com/u/26858253?v=4"/>1 member</span>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+                <li onClick={startLogout}><button className="button button--link">Logout</button></li>
+            </ul>
+          </div>
+      </div>
     </header>
 );
 
